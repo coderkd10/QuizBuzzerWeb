9598c0689ed77f9b1937d2b513aef3378b660316
Abhishek Kedia <kedia.abhishek10@gmail.com>
2016-02-18 04:00:41 +0530

    Add basic admin webinterface

---
+++ admin/demo.js
@@ -0,0 +1,35 @@
+var myFirebaseRef = new Firebase("https://quiz-buzzer.firebaseio.com/");
+var isQuestionActiveRef = myFirebaseRef.child("isQuestionActive");
+var isQuestionAnsweredRef = myFirebaseRef.child("isQuestionAnswered");
+
+function setQuestionActive() {
+	isQuestionAnsweredRef.set(false);
+	isQuestionActiveRef.set(true);
+}
+
+function setQuestionInactive() {
+	isQuestionActiveRef.set(false);
+	isQuestionAnswered.set(false);
+}
+
+isQuestionActiveRef.on("value", function(snapshot) {
+	var isActive = snapshot.val();
+	if (isActive) {
+		document.getElementById('questionStatus').innerHTML = 'Active';
+		document.getElementById('answeredStatusDiv').style.visibility = 'visible';
+	}
+	else {
+		document.getElementById('questionStatus').innerHTML = 'Inactive';
+		document.getElementById('answeredStatusDiv').style.visibility = 'hidden';
+	}
+});
+
+isQuestionAnsweredRef.on("value", function(snapshot) {
+	var isAnswered = snapshot.val();
+	if (isAnswered) {
+		document.getElementById('answerStatus').innerHTML = 'Yes';
+	}
+	else {
+		document.getElementById('answerStatus').innerHTML = 'No';
+	}
+});
